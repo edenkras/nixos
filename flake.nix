cd43a0b795f35e8ef8901dfaf632ce2872753873
Eden Krasnopolsky <eden.krasnopolsky@zeekit.me>
2023-11-16 20:25:30 +0200

    wip

--- flake.nix
+++ flake.nix
@@ -22,8 +22,6 @@
           home-manager.nixosModules.home-manager
           {
             home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
               extraSpecialArgs = { inherit inputs; };
               users.eden = import ./home-manager/home.nix;
             };
