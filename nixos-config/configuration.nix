4f46e50992b01f929db87b3665cded574b6f80da
Eden Krasnopolsky <eden.krasnopolsky@zeekit.me>
2023-11-17 20:22:38 +0200

    wip

--- nixos-config/configuration.nix
+++ nixos-config/configuration.nix
@@ -18,7 +18,7 @@
            version = "git-7ed4dd";
            src = prev.fetchFromGitHub {
              owner = "awesomewm";
-             repo = "awesomedbdbdb";
+             repo = "awesome";
              rev = "7ed4dd620bc73ba87a1f88e6f126aed348f94458";
              sha256 = "";
            };
