decabf87c768a6579c799a506370c04eb75036e5
Eden Krasnopolsky <eden.krasnopolsky@zeekit.me>
2023-11-17 20:23:26 +0200

    wip

--- nixos-config/configuration.nix
+++ nixos-config/configuration.nix
@@ -20,7 +20,7 @@
              owner = "awesomewm";
              repo = "awesome";
              rev = "7ed4dd620bc73ba87a1f88e6f126aed348f94458";
-             sha256 = "";
+             sha256 = "sha256-o69if8HQw2u0fp5J7jkS4WQeAXVuiFwpDLzGFscP4mM=";
            };
            patches = [];
          });
