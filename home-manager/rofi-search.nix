9f469b85f878deb91724fe3acb94946215245a2d
Eden Krasnopolsky <eden.krasnopolsky@zeekit.me>
2023-11-20 20:09:56 +0200

    wip

--- home-manager/rofi-search.nix
+++ home-manager/rofi-search.nix
@@ -5,6 +5,6 @@ pkgs.writeShellScriptBin "rofi-search" (builtins.readFile (
     owner = "fogine";
     repo = "rofi-search";
     rev = "1.1.0";
-    sha256 = "";
+    sha256 = "sha256-oGo6aJOdxxRmIeOI5Z6Ls4WyOsUEXFoCYaWeDpjW14g=";
   } + "/rofi-search"
 ))
