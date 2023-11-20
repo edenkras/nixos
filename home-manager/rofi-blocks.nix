65dcd1cedab426574c5080b4504e49aaf5133c7f
Eden Krasnopolsky <eden.krasnopolsky@zeekit.me>
2023-11-21 00:34:54 +0200

    wip

--- home-manager/rofi-blocks.nix
+++ home-manager/rofi-blocks.nix
@@ -10,7 +10,7 @@ in pkgs.stdenv.mkDerivation {
     rev = "0a2ba561aa9a31586c0bc8203f8836a18a1f664e";
     sha256 = "sha256-U955hzd55xiV5XdQ18iUIwNLn2JrvuHsItgUSf6ww58=";
   };
-  buildInputs = with pkgs; [ autoconf automake libtool pkg-config ];
+  buildInputs = with pkgs; [ autoconf automake libtool pkg-config json-glib ];
   buildPhase = ''
     autoreconf -i
     mkdir build
