d2314d048101481b0b8b39d98e5fdcd0f153ce7c
Eden Krasnopolsky <eden.krasnopolsky@zeekit.me>
2023-11-21 00:18:47 +0200

    wip

--- home-manager/rofi-blocks.nix
+++ home-manager/rofi-blocks.nix
@@ -8,6 +8,6 @@ in pkgs.stdenv.mkDerivation {
     owner = "OmarCastro";
     repo = "rofi-blocks";
     rev = "0a2ba561aa9a31586c0bc8203f8836a18a1f664e";
-    sha256 = "";
+    sha256 = "sha256-U955hzd55xiV5XdQ18iUIwNLn2JrvuHsItgUSf6ww58=";
   };
 }
