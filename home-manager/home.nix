ac269e328ec1ff5fa3804efd8dd0d3201ef54d52
Eden Krasnopolsky <eden.krasnopolsky@zeekit.me>
2023-11-20 18:04:54 +0200

    wip

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -45,7 +45,7 @@ in {
     rofi = {
       enable = true;
       theme = "Arc-Dark";
-      font = "Fira Code 18";
+      font = "Fira Code 16";
       extraConfig = {
          modes = "window,drun,ssh,power:rofi-power-menu,clipboard:greenclip print";
          ssh-command = "{terminal} {ssh-client} {host} [-p {port}]";
