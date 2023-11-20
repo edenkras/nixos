1c69d2245533395061aacb8d5137c67e5937d4d5
Eden Krasnopolsky <eden.krasnopolsky@zeekit.me>
2023-11-21 00:25:45 +0200

    wip

--- home-manager/rofi-blocks.nix
+++ home-manager/rofi-blocks.nix
@@ -10,4 +10,15 @@ in pkgs.stdenv.mkDerivation {
     rev = "0a2ba561aa9a31586c0bc8203f8836a18a1f664e";
     sha256 = "sha256-U955hzd55xiV5XdQ18iUIwNLn2JrvuHsItgUSf6ww58=";
   };
+  buildPhase = ''
+    autoreconf -i
+    mkdir build
+    cd build/
+    ../configure
+    make
+    make install
+  '';
+  installPhase = ''
+    mkdir -p $out/bin
+  '';
 }
